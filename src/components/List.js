8a66ad2f5c0901c20961a580694ee0da47e6a6f8
wtsaygers1 <wtsaygers1@gmail.com>
2021-03-22 23:48:38 -0400

    sandboxing the array objects and display list

---
+++ src/components/List.js
@@ -0,0 +1,20 @@
+// import React, { Component } from "react";
+
+// class List extends Component {
+//     createTasks(tasks) {
+//         return <li key={tasks.key}>{tasks.text}</li>
+//     }
+
+//     render() {
+//         let todoTasks = this.props.entries;
+//         let listItems = todoTasks.map(this.createTasks);
+
+//         return(
+//             <ul className="theList">
+//                 {listItems}
+//             </ul>
+//         );
+//     }
+// };
+
+// export default List;
