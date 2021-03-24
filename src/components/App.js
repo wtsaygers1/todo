0eaffa75888e8935649d703000614193fba04940
wtsaygers1 <wtsaygers1@gmail.com>
2021-03-24 16:19:31 -0400

    delete button functioning

--- src/components/App.js
+++ src/components/App.js
@@ -10,44 +10,59 @@ class App extends Component {
 
     this.state = {
       items: [],
+      inputElement: ""
     };
     // without the .bind(this) nothing is added to the items array
     this.addItem = this.addItem.bind(this);
+    this.deleteButton = this.deleteButton.bind(this);
+    this.checkButton = this.checkButton.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+
   addItem(e) {
     // what's up with the underscore infront of inputElement???
-    if (this._inputElement.value !== "") {
+    if (e.target.value !== "") {
       let newItem = {
-        text: this._inputElement.value,
+        text: this.state.inputElement,
         key: Date.now(),
         // how do I assign the id as the indexed position in the array -- is there a better practice??
         // is that the best practice??
         id: this.state.items.length,
-        click: false
+        checked: false,
+        deleted: false
       };
-      // working with the button
-      // completeButton(c) {
-      //   if (this.click === false) {
-      //     className = "bi bi-circle"
-      //   } else {
-      //     className = "bi check-circle"
-      //   }
-      // };
 
-      this.setState((prevState) => {
-        return {
-          // setState is taking the previous state of items which starts as an empty array and concatenates a
-          // new item into the array with each new submission
-          items: prevState.items.concat(newItem)
-        };
-      });
-      // inputs start as an empty string so the value can be updated with each new submission
-      this._inputElement.value = "";
+      // setState is taking the previous state of items which starts as an empty array and concatenates a
+      // new item into the array with each new submission
+      this.setState({
+         items: this.state.items.concat(newItem),
+         inputElement: ""
+        });      
     }
-
     // console.log(this.state.items);
     e.preventDefault();
   }
+
+  handleChange(event) {
+    this.setState({inputElement: event.target.value});
+  }
+
+  // eventHandler
+  deleteButton(key) {
+    console.log("clicked the function from app", key)
+    const deletedArr = this.state.items.map((item, index) => {
+      if (key === item.key) {
+        console.log("found the item", item, key);
+        item.deleted = true
+      }
+      return item
+    });
+    this.setState({ items: deletedArr })
+  }
+  checkButton() {
+    console.log("clicked the function from app")
+  }
+
 
   componentDidUpdate() {
     window.localStorage.setItem("items", JSON.stringify(this.state.items))
@@ -57,11 +72,12 @@ class App extends Component {
     let items = window.localStorage.getItem("items")
 
     if (items) {
-      this.setState( {items: JSON.parse(items)})
+      this.setState({ items: JSON.parse(items) })
     } else {
       window.localStorage.setItem("items", [])
     }
   }
+
 
   render() {
     return (
@@ -73,11 +89,15 @@ class App extends Component {
           {/* every time there is a new submission, the addItem method will run */}
           <form className="text-center" onSubmit={this.addItem}>
             <label>
-              <input type="text" name="name" ref={(a) => this._inputElement = a} placeholder="Input Task!" />
+              <input type="text" name="name" value={this.state.inputElement} onChange={this.handleChange} placeholder="Input Task!" />
             </label>
             <input type="submit" value="Submit" />
           </form>
-          <List tasks={this.state.items} />
+          <List
+            tasks={this.state.items}
+            deleteButton={this.deleteButton}
+            checkButton={this.checkButton}
+          />
         </div>
         <div className="BottomBar text-center">
           <BottomBar />
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,17 +1,14 @@
 import React from "react";
 
 function Task(props) {
-    // className="";
-
-window.localStorage.clear()
 
     return (
         <li className="border border-dark">
         {/* trying out this onClick inline function to turn click in addItem true in App.js */}
         {/* can try an alert as well to make sure button is firing */}
-            <button className="bi bi-circle"></button>
-            {props.text}
-            <button className="bi bi-x-circle"></button>
+            <button className="btn bi bi-circle" onClick={props.checkButton}></button>
+            {props.task.text}
+            <button className="btn bi bi-x-circle" onClick={() => props.deleteButton(props.task.key)}></button>
         </li>
     );
 }
